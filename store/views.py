c368883d6d2e5b85fae94189f99d23633aeea3a3
allen <martinallen722@gmail.com>
2020-07-14 08:39:54 +0000

    The structure of my e-commerce data

--- store/views.py
+++ store/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
+from . models import *
 
 def store(request):
-    return render(request, 'store.html', {'store':store})
+    products = Product.objects.all()
+    return render(request, 'store.html', {'products':products})
 
 def cart(request):
-    return render(request, 'cart.html', {'cart':cart})
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items= order.orderitem_set.all()
+
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    return render(request, 'cart.html', {'items':items,'order':order})
 
 def checkout(request):
-    return render(request, 'checkout.html', {'checkout':checkout})
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items= order.orderitem_set.all()
+
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    return render(request, 'checkout.html', {'items':items, 'order':order})
